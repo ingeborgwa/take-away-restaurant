57254024545a783a813434f8aae3ce470e439c1e
ingeborgwa <ingeborg.austad@gmail.com>
2021-03-11 16:41:53 +0100

    added remove from cart btn

---
+++ contexts/BasketContext.js
@@ -0,0 +1,48 @@
+import React, { createContext, useContext, useEffect, useState } from 'react';
+
+
+const BasketContext = createContext({
+  productLines: [],
+  addProductLine: () => {},
+  total: 0,
+});
+
+export const Basket = ({ children }) => {
+  const [productLines, setProductLines] = useState([]);
+  const [total, setTotal] = useState(0);
+
+  const addProductLine = (product) => {
+    setProductLines([...productLines, product]);
+  };
+
+
+
+  //Remove from basket
+  const removeProductLine = (id) => {
+    let filteredProductLines = productLines.filter((item) => item.id !== id);
+    setProductLines(filteredProductLines);
+  };
+
+  
+
+
+  useEffect(() => {
+    const total = productLines.reduce((prev, cur) => {
+        console.log("Lagt til", productLines);
+      return prev + cur.pris;
+    }, 0);
+    setTotal(total);
+  }, [productLines]);
+
+  return (
+    <BasketContext.Provider value={{ productLines, addProductLine, total, removeProductLine }}>
+      {children}
+    </BasketContext.Provider>
+  );
+};
+
+export const BasketConsumer = BasketContext.Consumer;
+
+export const useBasket = () => {
+  return useContext(BasketContext);
+};
